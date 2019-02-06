294af883be63fa02812c3c24e82f039338229d3f
Bernard <laubernardsy@gmail.com>
2019-02-05 23:35:50 -0500

    Generated movie review data Generated user data Added movie review and user data into database

--- server/database/index.js
+++ server/database/index.js
@@ -1,14 +1,48 @@
 var mysql = require('mysql');
+var movieReviews = require('../../data/movieReviews.json');
+var users = require('../../data/user.json');
 
 var connection = mysql.createConnection({
   host : 'localhost',
   user: 'root',
-  password:''
+  password:'',
+  database: 'reviews'
 });
 
 connection.connect(function(err){
-   if(err){
+   if (err){
       console.log(err);
    }
-  console.log("Connected...");  
- });
+   console.log("Connected...");
+   
+   // Insert movie reviews
+   // TODO: Insert Date...currently is null
+   movieReviews.forEach( (review) => {
+      // var sql = "INSERT INTO audience_reviews (id, review, user_id, movie_id, stars, not_interested, want_to_see_it) VALUES (" + review.id + "," + mysql.escape(review.review) + "," + review.user_id + "," + review.movie_id + "," + review.stars + "," + review.not_interested + "," + review.want_to_see_it + ")";
+      var sql = `INSERT INTO audience_reviews (id, review, user_id, \
+               movie_id, stars, not_interested, want_to_see_it) VALUES (${review.id}, \
+               ${mysql.escape(review.review)}, ${review.user_id}, ${review.movie_id}, \
+               ${review.stars}, ${review.not_interested}, ${review.want_to_see_it})`;
+      connection.query(sql, (err, res) => {
+         if (err) {
+            console.log("err writing", err);
+         }
+         console.log("record was inserted");
+      });
+   });
+
+   // Insert users
+   // TODO: Insert etag, objectURL for profile pic
+   users.forEach( (user) => {
+      var sql = `INSERT INTO users (user_id, username, has_profile_pic \
+         ) VALUES (${user.id}, \
+         ${mysql.escape(user.username)}, ${user.has_profile_pic})`;
+      connection.query(sql, (err, res) => {
+         if (err) {
+            console.log("err writing", err);
+         }
+         console.log("record was inserted");
+      });
+   });
+   
+});
